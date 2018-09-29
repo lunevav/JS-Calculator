84cc0045cb15992441bf98b040059b5c3e5c382b
Aleksandr Lunev <alexander.lunev@gmail.com>
2018-09-29 18:26:29 +0300

    dev branch added

--- calcEngine.js
+++ calcEngine.js
@@ -1,30 +1,25 @@
 const inputScreen = document.getElementById('screen');
 const btnPanel = document.getElementById('btn-panel');
 // TODO 3 PANELS
-
 class Screen {
     constructor(value = 0) {
         this.value = value;
     }
-
     showValue(btnValue) {
         inputScreen.value = btnValue;
     }
 }
-
 class Buttons {
     constructor(value, type, cssClass) {
         this.value = value;
         this.type = type;
         this.cssClass = cssClass;
     }
-
     click() {
         console.info(this.value + " " + this.type);
         new Screen(this.value).showValue(this.value);
         return this.value;
     }
-
     drawButton() {
         let elem = document.createElement("button");
         elem.className = this.cssClass;
@@ -34,7 +29,6 @@ class Buttons {
         return elem;
      }
 }
-
 class RenderButtons {
     renderDefaultUI(ab) {
         for (let i = 0; i < ab.length; i++) {
@@ -42,7 +36,6 @@ class RenderButtons {
         }
     }
 }
-
 const arrayOfButton = [
     new Buttons(0, "digit", "btn"),
     new Buttons(1, "digit", "btn"),
@@ -52,7 +45,5 @@ const arrayOfButton = [
     new Buttons(5, "digit", "btn"),
     new Buttons(6, "digit", "btn")
 ]
-
 var runCalc = new RenderButtons();
-
 runCalc.renderDefaultUI(arrayOfButton);
